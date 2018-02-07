6054bac001f7e09c9ae673cc61336000f769d6b7
Thibaud Chupin <thibaud.chupin@gmail.com>
2018-02-07 11:17:55 +0100

    Automatically start `roscore` on`Gru`

--- bash/bin/bash/host-settings/gru.bash
+++ bash/bin/bash/host-settings/gru.bash
@@ -1,2 +1,22 @@
 
 export ROS_MASTER_URI="http://gru.local:11311"
+
+# {enable,disable}-davinci-monitors.bash scripts to PATH
+PATH=/srv/ros_ws/scripts/:${PATH}
+
+echo -e ""
+
+# Try to see if roscore is running 
+rostopic list > /dev/null 2>&1
+if [[ "$?" != "0" ]]; then 
+    exec roscore > /dev/null 2>&1 &! 
+    
+    # Ideally we would test to see if the roscore is up but for that we would
+    # have to wait a little for it to finish setup. To avoid delaying startup 
+    # too much we'll just hope that it comes up.
+    echo -e " ${GREEN}✔${RESET} ${BOLD}roscore${RESET} started as PID ${BOLD}$!${RESET}"
+else 
+    echo -e " ${GREEN}✔${RESET} ${BOLD}roscore${RESET} running"
+fi
+
+echo -e ""
