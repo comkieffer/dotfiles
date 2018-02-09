27a4032ebdd9e5e6ec96f4daa988c2595bc31e31
Thibaud Chupin <thibaud.chupin@gmail.com>
2018-02-09 16:17:16 +0100

    Make sure that we're not the ROS master

--- bash/bin/ros_enter_env.bash
+++ bash/bin/ros_enter_env.bash
@@ -11,7 +11,8 @@ source rossrc
 ## Locate a ROS Master on the network or start one
 
 # By convention the ROS master server is at 192.168.1.1 on the network. If it doesn't answer we 
-# should assume that we are alone and should create our own master.
+# should assume that we are alone and should create our own master. 
+# However if it does answer we need to check that we're not hosting the roscore
 _ROSCORE_SERVER_IP='192.168.1.1'
 
 # Try to ping the roscore server to see if it is available
@@ -19,32 +20,34 @@ timeout 0.5 ping ${_ROSCORE_SERVER_IP} -c 1 > /dev/null 2>&1
 _FOUND_ROSCORE_SERVER=$?;
 
 if [[ ${_FOUND_ROSCORE_SERVER} == 0 ]] ; then
-    
     # Resolve the hostname of the roscore server and store it
     _ROSCORE_SERVER_NAME=$(avahi-resolve-address ${_ROSCORE_SERVER_IP} | cut -f 2);
-    echo -e "  ${GREEN_TICK} Connected to \e[1mROS_MASTER\e[0m on \e[1m${ROSCORE_SERVER_NAME}\e[0m (\e[1m${ROS_MASTER_URI}\e[0m)"  
 
-    export ROS_MASTER_URI=http://${_ROSCORE_SERVER_IP}:11311;
+    if [[ ${_ROSCORE_SERVER_NAME} != "$(hostname -s).local" ]]; then
+        echo -e "  ${GREEN_TICK} Connected to ${BOLD}ROS_MASTER${RESET} on ${BOLD}${_ROSCORE_SERVER_NAME}${RESET} (${BOLD}${ROS_MASTER_URI}${RESET})"  
+        export ROS_MASTER_URI=http://${_ROSCORE_SERVER_IP}:11311;
 
-    # Test the master
-    rostopic list > /dev/null 2>&1
-    if [[ $? == "1" ]]; then 
-        echo -e "  ${RED_CROSS} Unable to communicate with master!"
+        # Test the master
+        rostopic list > /dev/null 2>&1
+        if [[ $? == "1" ]]; then 
+            echo -e "  ${RED_CROSS} Unable to communicate with master!"
+        fi
     fi
 
     unset _ROSCORE_SERVER_NAME
 else 
     # If we haven't found a  roscore server we start our own roscore in the background
-    rostopic list > /dev/null 2>&1
-
+    # 
     # The exit status of 'rostopic list' tells us if it was able to connect to the master. 
     # This is the simplest way of finding out if a roscore server is runnning short of grepping the
     # output of ps. 
+    rostopic list > /dev/null 2>&1
+
     if [[ $? == "1" ]]; then 
         exec roscore > /dev/null 2>&1 &!
-        echo -e "  ${GREEN_TICK} Starting roscore on \e[1mlocalhost\e[0m with PID \e[1m$!\e[0m"
+        echo -e "  ${GREEN_TICK} Starting roscore on ${BOLD}localhost${RESET} with PID ${BOLD}$!${RESET}"
     else
-        echo -e "  ${YELLOW_WARNING} It looks like roscore is already running. \e[1mNothing to start.\e[0m" 
+        echo -e "  ${YELLOW_WARNING} It looks like roscore is already running. ${BOLD}Nothing to start.${RESET}" 
     fi
 fi
 
@@ -54,10 +57,3 @@ unset _ROSCORE_SERVER_IP _FOUND_ROSCORE_SERVER
 export ROSCONSOLE_FORMAT='[${severity}] <${time}> ${node} @ ${file}:${line}
     ${message}'
 
-# Source the completions file
-if [[ -f /opt/ros/${ROS_DISTRO}/share/rosbash/rosbash ]]; then
-    source /opt/ros/${ROS_DISTRO}/share/rosbash/rosbash
-    echo -e "  ${GREEN_TICK} Completions Loaded"
-else 
-    echo -e "  ${RED_CROSS} Unknown ROS distribution. Unable to load completions"
-fi
