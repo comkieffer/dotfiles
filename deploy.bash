b3346f16b8cf82065203ea050791eb544946ae3d
Thibaud Chupin <thibaud.chupin@gmail.com>
2018-03-12 12:44:20 +0100

    Add _Sublime Text 3_ settings to the repo

--- deploy.bash
+++ deploy.bash
@@ -11,6 +11,7 @@ STOW_COMMAND="stow --target ${INSTALLATION_DIR} --verbose --restow"
 ${STOW_COMMAND} bash 
 ${STOW_COMMAND} vim
 ${STOW_COMMAND} git
+${STOW_COMMAND} sublime-text-3
 
 # Keep the ssh directory separate so that I don't accidentally commit all my keys
 if [[ -d ssh/ ]]; then
