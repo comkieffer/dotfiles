9416258bc760a7cb59f36a860389700beedfebc2
Thibaud Chupin <thibaud.chupin@spaceapplications.com>
2019-05-29 11:37:20 +0200

    Fix problems in deploy script
    - Fix the issue where large directories were being symlinked (e.g.
    ~/bin/, ~/.config/) which caused problems later on when we would add new files to
    them. We now create these directories manually before calling stow.
    
    - Improve progress messsages and error detection

--- deploy.bash
+++ deploy.bash
@@ -8,13 +8,32 @@ set -e
 INSTALLATION_DIR="${HOME}"
 STOW_COMMAND="stow --target ${INSTALLATION_DIR} --verbose --restow"
 
-${STOW_COMMAND} bash 
-${STOW_COMMAND} vim
-${STOW_COMMAND} git
-${STOW_COMMAND} sublime-text-3
+function install {
+    echo ""
+    echo "Installing $1 files ... "
+    $STOW_COMMAND $1
+}
+
+# Start by creating the directories we need. 
+# If we don't create bin/ for example it will be simlinked in which will be a 
+# hassle easch time we add a script to it
+mkdir -p ~/bin/
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/sublime-text-3
+
+# Install dotfiles and scripts ... 
+
+install bash
+install vim 
+install git 
+install sublime-text-3
+install i3
+
 
 # Keep the ssh directory separate so that I don't accidentally commit all my keys
+echo ""
 if [[ -d ssh/ ]]; then
+    echo "Installing ssh files ..."
     ${STOW_COMMAND} ssh
 else    
     echo "Skipping missing ssh/ directory"
@@ -23,6 +42,7 @@ fi
 # Tilix is different:
 #   Colour schemes are stored in .config/Tilix/schemes
 #   General config is stored in dconf 
-dconf load /com/gexperts/Tilix/  tilix/tilix_dconf.txt
+# dconf load /com/gexperts/Tilix/  tilix/tilix_dconf.txt
 
+set +e
 unset INSTALLATION_DIR STOW_COMMAND
