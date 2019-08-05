22b2049f1b21d7f5d54faa9fbb6b9ebb7956069f
Thibaud Chupin <thibaud.chupin@spaceapplications.com>
2019-08-05 11:29:57 +0200

    Install direnv from system pacakges.
    The previous version has a bug which overwrites the exit status of the last executed command by prepending itself to "PROMPT_COMMAND".

--- installers/install-common-packages.sh
+++ installers/install-common-packages.sh
@@ -5,7 +5,7 @@ sudo apt-get update > /dev/null
 sudo apt-get upgrade -y > /dev/null
 
 echo "Installing common packages ..."
-sudo apt-get install -qq build-essential htop stow > /dev/null
+sudo apt-get install -qq build-essential htop stow direnv > /dev/null
 
 has_i3=$(which i3 > /dev/null; echo $?)
 if [ $has_i3 -eq 0 ]; then 
