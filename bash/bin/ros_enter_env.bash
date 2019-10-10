903a3d2ad37c321e1ca9bb36f3caf7260120426b
Thibaud Chupin <thibaud.chupin@spaceapplications.com>
2019-10-10 14:44:49 +0200

    Improve `ros_enter_env` to automatically start a `roscore` if needed

--- bash/bin/ros_enter_env.bash
+++ bash/bin/ros_enter_env.bash
@@ -9,14 +9,7 @@ echo "Setting up ROS environment ... "
 ## Get the full path to the setup.bash file
 source _rossrc_cmd
 
-#
-# We used to do more things in here like automatically starting a roscore 
-# instance but that just adds complexity to somthing that shouldn't be 
-# complicated.
-#
-# The most important thing is sourcing the proper environment.
-#
-# If you need to do other things you can add them here.
-#
-
-
+if ! ros_master_running; then
+    roscore > /dev/null 2>&1 &!
+    echo -e "  ${GREEN_TICK} Started new roscore with PID $!"
+fi
