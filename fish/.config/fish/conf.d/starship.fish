d3567e996b3f100edc8942c991f10041d6791cd5
Thibaud Chupin <thibaud.chupin@ext.esa.int>
2023-03-02 11:47:27 +0100

    fish: Add `starship` prompt

---
+++ fish/.config/fish/conf.d/starship.fish
@@ -0,0 +1,10 @@
+# Starfish is the framework used to generate the prompt
+# Install it from https://starship.rs/
+#
+# Or run
+#   curl -sS https://starship.rs/install.sh | sh
+
+
+if type -q starship
+    starship init fish | source
+end
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,11 +2,6 @@
 # Add common excutable paths to $PATH
 fish_add_path ~/.local/bin/
 
-# Starfish is the framework used to generate the prompt
-# Install it from https://starship.rs/
-if type -q starship
-    starship init fish | source
-end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
