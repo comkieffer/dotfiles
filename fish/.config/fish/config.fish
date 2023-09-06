2389dc2e59f3eb426470589d005602e4815d6a4e
Thibaud Chupin <thibaud.chupin@ext.esa.int>
2023-09-06 11:05:58 +0200

    fish: Add aliases for color support

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,17 @@ fish_add_path ~/.local/bin/
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    alias ip "ip --color=auto"
+
+    # -A show all dotfiles except . and ..
+    # -b use c-style escapes instead of quoting
+    # -1 (one): list (use -l for long version)
+    # -F classify entries with (*/=>@|)
+    # -h: human-readable sizes
+    # -v: natural sort of number
+    # -T 0 do not use tabs for alignment
+
+    alias ls='ls --color=auto --time-style=long-iso --group-directories-first -Ab1Fhv -T 0'
 end
 
 alias dotfiles "git --git-dir=$HOME/Projects-Personal/homedir --work-tree=$HOME"
