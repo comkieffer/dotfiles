68faa2c03122387af09452cf355c9062a61bee14
Thibaud Chupin <thibaud.chupin@gmail.com>
2023-12-21 09:31:36 +0100

    Fix dot command for fish

--- fish/.config/fish/functions/dot.fish
+++ fish/.config/fish/functions/dot.fish
@@ -6,9 +6,9 @@ function dot
     if test (count $argv) -eq 0
         begin
             cd "$HOME"
-            for i in (dotfiles ls-files); do
-                echo -n "(dotfiles -c color.status=always status $i -s | sed "s#$i##")"
-                echo -e "¬/$i¬\e[0;33m(dotfiles -c color.ui=always log -1 --format="%s" -- $i)\e[0m"
+            for i in (dotfiles ls-files);
+                echo -n (dotfiles -c color.status=always status $i -s | sed "s#$i##")
+                echo -e "¬/$i¬\e[0;33m"(dotfiles -c color.ui=always log -1 --format="%s" -- $i)"\e[0m"
             end
         end | column -t --separator=¬ -T2
     else
