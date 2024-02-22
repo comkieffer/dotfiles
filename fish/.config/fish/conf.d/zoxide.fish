40d30fb8e370e61f248fb44ac060bfbc949d72c2
Thibaud Chupin <thibaud.chupin@gmail.com>
2024-02-22 10:49:41 +0100

    fish: document `zoxide` install process

--- fish/.config/fish/conf.d/zoxide.fish
+++ fish/.config/fish/conf.d/zoxide.fish
@@ -1,8 +1,15 @@
 # Required `zoxide` package install with
 #   apt install zoxide
 #
+# Alternatively, use the install script from the repository to download the 
+# binary directly
+#
+#  curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+#
 # Provides the `z` command to navigate the directory tree
 
-if type -q zoxide;
-    zoxide init fish | source
+if status is-interactive
+    if type -q zoxide;
+        zoxide init fish | source
+    end
 end
