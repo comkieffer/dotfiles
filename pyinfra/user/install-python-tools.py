6a3d64badb319a19402799bea6dd7292a72a2bdf
Thibaud Chupin <thibaud.chupin@gmail.com>
2026-03-01 16:41:33 +0100

    pyinfra: Update the list of Python tools

--- pyinfra/user/install-python-tools.py
+++ pyinfra/user/install-python-tools.py
@@ -1,13 +1,28 @@
 from pyinfra import host
 from pyinfra.facts.server import Which
-from pyinfra.operations import pipx
+from pyinfra.operations import pipx, server
 
 pipx.packages(
     name="Install packages with pipx",
     packages=[
         "black",
-        "ruff",
+        "copier",
+        "poetry",
         "pre-commit",
+        "ruff",
+        "sphinx",
+        "uv",
+    ]
+)
+
+# Inject sphinx extensions into the sphinx pipx environment.
+# Since pyinfra doesn't have a pipx.inject operation, we use shell commands to add
+# the extensions to sphinx's isolated environment, matching the setup in docker/sphinx.dockerfile.
+server.shell(
+    name="Inject sphinx extensions",
+    commands=[
+        "pipx inject --include-apps sphinx sphinx-autobuild",
+        "pipx inject sphinx sphinx-copybutton sphinx-design sphinx-mermaid sphinx-subfigure 'myst-parser[linkify]' furo",
     ]
 )
 
