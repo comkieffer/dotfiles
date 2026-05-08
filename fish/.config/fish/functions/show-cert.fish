843e22a6ce48182de45be8321100b8a6ece387bf
Thibaud Chupin <thibaud.chupin@gmail.com>
2026-05-08 18:47:49 +0200

    fish: show-cert supports local certificates

--- fish/.config/fish/functions/show-cert.fish
+++ fish/.config/fish/functions/show-cert.fish
@@ -1,9 +1,20 @@
 function show-cert \
   --description 'Download a certificate and display it'
 
+  if test (count $argv) -ne 1
+    echo Usage: (status function) CERTIFICATE_OR_HOST
+    return 1
+  end
+
+
+  if test -f $argv[1]
+    openssl x509 -noout -text -in $argv[1]
+    return $status
+  end
+
   # If the url has http:// or https:// strip it.
   # The `openssL_host` variable contains the rest of the string.
-  string match --regex '^(?:https?:\/\/)?(?<openssl_host>.+)' $argv > /dev/null;
+  string match --regex '^(?:https?:\/\/)?(?<openssl_host>.+)' $argv[1] > /dev/null;
 
   echo "Downloading certificate from $openssl_host ..."
   echo -n \
