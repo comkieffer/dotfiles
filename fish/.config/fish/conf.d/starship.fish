92ee73ce01f8990ee71ff2baef34380a178fbeee
Thibaud Chupin <thibaud.chupin@gmail.com>
2026-05-24 23:11:49 +0200

    fish: Update starship config
    Improve readability of git status, background jobs, directory path, ...

--- fish/.config/fish/conf.d/starship.fish
+++ fish/.config/fish/conf.d/starship.fish
@@ -5,4 +5,13 @@
 
 if type -q starship;
     starship init fish | source
+
+    # React to STARSHIP_JOBS changes to export job names for the prompt.
+    function __update_fish_job_names --on-variable STARSHIP_JOBS
+        if test "$STARSHIP_JOBS" -gt 0 2>/dev/null
+            set -gx FISH_JOB_NAMES (jobs 2>/dev/null | awk 'NR>1 {print $5}' | string join ', ')
+        else
+            set -e FISH_JOB_NAMES
+        end
+    end
 end
